5ad902eb5496702faf97d15c3d445da8a05545a9
atlas <kefirzefir523@gmail.com>
2025-06-15 14:14:40 +0500

    aqaq

--- home/programs/emacs.nix
+++ home/programs/emacs.nix
@@ -1,36 +1,19 @@
 { config, pkgs, ... }:
 
-let
-  doomDir = "${config.home.homeDirectory}/.doom.d";
-in {
+{
   home.packages = with pkgs; [
     emacs
-    fd   
-    ripgrep 
+    fd
+    ripgrep
   ];
-
-home.file.".emacs.d".source = pkgs.fetchFromGitHub {
-  owner = "doomemacs";
-  repo = "doomemacs";
-  rev = "e6c755305358412a71a990fc2cf592c629edde1e";
-  sha256 = "1f5k6mhrdcfsq1gqc0m36f1zvsi9fwi4cmrvf6jfaf3q9bpyalbg";
-};
-
 
   home.file.".doom.d/config.el".text = ''
     (setq user-full-name "atlas"
           user-mail-address "user@example.com")
 
-    ;; Общая тема интерфейса
     (setq doom-theme 'doom-one)
-
-    ;; Увеличить шрифт
     (setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 14))
-
-    ;; Файлы по умолчанию открываются в текущей директории
     (setq-default default-directory "~/")
-
-    ;; Включить относительные номера строк
     (setq display-line-numbers-type 'relative)
   '';
 
