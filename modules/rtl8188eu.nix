9bdba2fde42bb1601be098fcbbdd1416ae547b4d
atlas <kefirzefir523@gmail.com>
2025-07-21 21:44:41 +0500

    driver wifif

--- modules/rtl8188eu.nix
+++ modules/rtl8188eu.nix
@@ -1,22 +1,33 @@
 { config, pkgs, lib, ... }:
 
 let
+  kernel = config.boot.kernelPackages.kernel;
   rtl8188euDrv = pkgs.stdenv.mkDerivation {
-    name = "rtl8188eu";
+    pname = "rtl8188eu";
+    version = "unstable";
+
     src = pkgs.fetchFromGitHub {
       owner = "lwfinger";
       repo = "rtl8188eu";
-      rev = "master";
-      sha256 = "0000000000000000000000000000000000000000000000000000"; # заменить
+      rev = "HEAD"; # можно указать конкретный коммит
+      sha256 = "0000000000000000000000000000000000000000000000000000"; # будет ошибка с актуальной хеш-суммой
     };
-    nativeBuildInputs = with pkgs; [ kernelPackages.kernel.dev pkgs.git ];
+
+    nativeBuildInputs = with pkgs; [ config.boot.kernelPackages.kernel.dev config.boot.kernelPackages.kernelHeaders pkgs.git ];
+
     buildPhase = ''
-      make
+      make ARCH=x86_64 KSRC=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build
     '';
+
     installPhase = ''
-      mkdir -p $out/lib/modules/${pkgs.kernel.version}/kernel/drivers/net/wireless
-      cp 8188eu.ko $out/lib/modules/${pkgs.kernel.version}/kernel/drivers/net/wireless/
+      mkdir -p $out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/wireless
+      cp 8188eu.ko $out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/wireless/
     '';
+
+    meta = {
+      description = "Realtek RTL8188EU driver";
+      license = lib.licenses.gpl2;
+    };
   };
 in {
   boot.extraModulePackages = [ rtl8188euDrv ];
